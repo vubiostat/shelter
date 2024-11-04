992ea27ea96903431cc5d9eea7198d46d8037522
Shawn Garbett <shawn@garbett.org>
2024-11-04 09:05:12 -0600

    Fixed test mock to new interfface

--- tests/testthat/test-103-unlockAlgorithm.R
+++ tests/testthat/test-103-unlockAlgorithm.R
@@ -13,7 +13,7 @@ test_that(
     stub(.unlockAlgorithm, ".unlockKeyring", TRUE)
     stub(.unlockAlgorithm, "key_list",
          data.frame(service="redcapAPI", username="Nadda"))
-    stub(.unlockAlgorithm, "key_set_with_value", m)
+    stub(.unlockAlgorithm, "key_set", m)
 
     calls <- 0
     passwordFUN <- function(...) {calls <<- calls + 1; "xyz"}
@@ -30,7 +30,7 @@ test_that(
     expect_true("rcon" %in% names(x))
     expect_true(x$rcon)
     expect_called(m, 1) # Called key_set_with_value once
-    expect_equal(mock_args(m)[[1]], list("API_KEYs", "shelter", "George", "xyz" ))
+    expect_equal(mock_args(m)[[1]], list("API_KEYs", "George", "xyz" ))
     expect_called(n, 1) # Called connectFUNs
   }
 )
