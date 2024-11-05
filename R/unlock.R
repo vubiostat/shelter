1c75bbfb26082d0582d75d273e8a8ba5ed1b3660
Shawn Garbett <shawn@garbett.org>
2024-11-04 20:09:29 -0600

    Merge from redcapAPI https://github.com/vubiostat/redcapAPI/pull/419

--- R/unlock.R
+++ R/unlock.R
@@ -165,8 +165,8 @@
 #' @importFrom getPass getPass
 .default_pass <- function()
 {
-  if(grepl('mac', tolower(utils::osVersion))        &&
-     requireNamespace("rstudioapi", quietly = TRUE) &&
+  if(isTRUE(grepl('mac', tolower(utils::osVersion))) &&
+     requireNamespace("rstudioapi", quietly = TRUE)  &&
      rstudioapi::isAvailable(child_ok=TRUE))
   {
     rstudioapi::askForPassword
