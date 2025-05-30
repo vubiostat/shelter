4cdce69d14ba6da8a405105a3585667913b1d126
Shawn Garbett <shawn@garbett.org>
2025-05-30 14:45:13 -0500

    cleanup copyright #10

--- R/unsafe_export.R
+++ R/unsafe_export.R
@@ -1,5 +1,4 @@
-# Copyright (C) 2025 Vanderbilt University
-# Shawn Garbett, Cole Beck, Hui Wu, Benjamin Nutter, Savannah Obregon
+# Copyright (C) 2025 Vanderbilt University, Shawn Garbett, Cole Beck
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
